0f395f3873973373646b86955ebff0448919b579
Talia Koch <kochtz@yahoo.com>
2018-02-05 09:50:53 -0800

    Initial Commit

---
+++ EvacuAppTests/EvacuAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  EvacuAppTests.swift
+//  EvacuAppTests
+//
+//  Created by Talia Koch on 2/5/18.
+//  Copyright © 2018 Talia Koch. All rights reserved.
+//
+
+import XCTest
+@testable import EvacuApp
+
+class EvacuAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
